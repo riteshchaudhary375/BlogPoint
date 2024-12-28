d2fa8e9d01a93671b56fe8a6bce8fc2532dd3547
ritesh <riteshchaudhary375@gmail.com>
2024-12-28 21:27:08 +0545

    create pages and route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,37 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+import CreatePost from "./pages/CreatePost";
+import UpdatePost from "./pages/UpdatePost";
+import PostPage from "./pages/PostPage";
+import Search from "./pages/Search";
+
 function App() {
-  return <h1 className="text-3xl font-bold">Hello World!</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/post/:postSlug" element={<PostPage />} />
+        <Route path="/search" element={<Search />} />
+
+        {/* Private Route */}
+        <Route path="/dashboard" element={<Dashboard />} />
+
+        {/* Admin Route */}
+        <Route path="/create-post" element={<CreatePost />} />
+        <Route path="/update-post/:postId" element={<UpdatePost />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
