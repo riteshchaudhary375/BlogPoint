7d3f9d4d05f99c97ede718858cf528e55bc77ea6
ritesh <riteshchaudhary375@gmail.com>
2025-01-03 22:12:02 +0545

    create express app and connect to mongo database

---
+++ api/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
