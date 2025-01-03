dde18a2ffbf40c3cedbbe62de5781e1fe91ae257
ritesh <riteshchaudhary375@gmail.com>
2025-01-03 22:17:23 +0545

    add error middleware and error handler function for error handling

--- api/index.js
+++ api/index.js
@@ -23,3 +23,15 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
+
+// Middleware for error handle
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal server error";
+
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
