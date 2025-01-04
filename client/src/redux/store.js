f067dacbf7e6cdb9ce76b90181b49fb03e304885
ritesh <riteshchaudhary375@gmail.com>
2025-01-04 21:08:02 +0545

    add redux-persist to store user login data

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,13 +1,24 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
-
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import userReducer from "./user/userSlice.js";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
 
 const rootReducers = combineReducers({
   user: userReducer,
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducers);
+
 export const store = configureStore({
-  reducer: rootReducers,
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({ serializableCheck: false }),
 });
+
+export const persistor = persistStore(store);
