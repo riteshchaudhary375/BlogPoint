8bc85180d802fa576201d7db875a7c0758c7dd8d
ritesh <riteshchaudhary375@gmail.com>
2025-01-05 21:22:57 +0545

    add sign out api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,13 @@
+// Test user
 export const test = async (req, res) => {
   res.json({ message: "API running" });
+};
+
+// Sign out
+export const signout = async (req, res, next) => {
+  try {
+    res.clearCookie("access_token").status(200).json("Logged out successfully");
+  } catch (error) {
+    next(error);
+  }
 };
