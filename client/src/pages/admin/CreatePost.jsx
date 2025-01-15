3a3b45842e3708be6a2e5cb2ab1e33a6e2d4d88c
ritesh <riteshchaudhary375@gmail.com>
2025-01-15 18:50:28 +0545

    complete new create post by admin

--- client/src/pages/admin/CreatePost.jsx
+++ client/src/pages/admin/CreatePost.jsx
@@ -1,43 +1,94 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import ReactQuill from "react-quill-new";
 import "react-quill-new/dist/quill.snow.css";
 
 import { assets } from "../../assets/assets";
 import Title2 from "../../components/Title2";
 import Button from "../../components/Button";
+import toast from "react-hot-toast";
 
 const CreatePost = () => {
+  const [image, setImage] = useState(false);
+  const filePickerRef = useRef();
+
+  const [title, setTitle] = useState("");
+  const [category, setCategory] = useState("uncategorized");
+  const [content, setContent] = useState("");
+
+  const [publishError, setPublishError] = useState(null);
+
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+
+      const formData = new FormData();
+
+      formData.append("title", title);
+      formData.append("category", category);
+      formData.append("content", content);
+
+      image && formData.append("image", image);
+
+      const res = await fetch(`/api/post/create`, {
+        method: "POST",
+        body: formData,
+      });
+      const data = await res.json();
+      if (!res.ok || data.success === false) {
+        // toast.error(data.message);
+        setPublishError(data.message);
+        return;
+      }
+      if (res.ok) {
+        toast.success(data.message);
+        setTitle("");
+        setContent("");
+        setImage(false);
+      }
+    } catch (error) {
+      // toast.error(error.message);
+      setPublishError(error.message);
+    }
+  };
+
   return (
     <div className="w-full lg:w-[800px]">
       <div className="text-center">
         <Title2 text1={"Create a"} text2={"Post"} />
       </div>
 
-      <form className="flex flex-col gap-4">
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
         <div className="flex flex-col gap-2 lg:grid grid-cols-[3fr_1fr] lg:gap-4">
           <input
             type="text"
             id="title"
-            placeholder="Title"
+            placeholder="Title of the post"
             className="bg-inherit border border-borderColor outline-borderColorHover rounded-sm px-2 py-1"
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+            required
           />
+
           <div className="flex justify-end">
-            <select className="bg-inherit border border-borderColor outline-borderColorHover rounded-sm px-2 py-1">
-              <option value={"uncategorized"}>Select a category</option>
-              <option value={"technology"}>Technology</option>
-              <option value={"health"}>Health</option>
-              <option value={"finance"}>Finance</option>
-              <option value={"travel"}>Travel</option>
-              <option value={"lifestyle"}>Lifestyle</option>
-              <option value={"food"}>Food</option>
-              <option value={"education"}>Education</option>
-              <option value={"businessEntrepreneurship"}>
+            <select
+              className="bg-inherit border border-borderColor outline-borderColorHover rounded-sm px-2 py-1 cursor-pointer"
+              onChange={(e) => setCategory(e.target.value)}
+            >
+              <option value={"Uncategorized"}>Select a category</option>
+              <option value={"Technology"}>Technology</option>
+              <option value={"Health"}>Health</option>
+              <option value={"Finance"}>Finance</option>
+              <option value={"Travel"}>Travel</option>
+              <option value={"Lifestyle"}>Lifestyle</option>
+              <option value={"Food"}>Food</option>
+              <option value={"Education"}>Education</option>
+              <option value={"Business & Entrepreneurship"}>
                 Business & Entrepreneurship
               </option>
-              <option value={"entertainment"}>Entertainment</option>
-              <option value={"parentingFamily"}>Parenting & Family</option>
-              <option value={"selfImprovement"}>Self-Improvement</option>
-              <option value={"sportsFitness"}>Sports & Fitness</option>
+              <option value={"Entertainment"}>Entertainment</option>
+              <option value={"Parenting & Family"}>Parenting & Family</option>
+              <option value={"Self-Improvement"}>Self-Improvement</option>
+              <option value={"Sports & Fitness"}>Sports & Fitness</option>
             </select>
           </div>
         </div>
@@ -45,12 +96,24 @@ const CreatePost = () => {
         <div
           className="w-full h-[200px] border border-borderColor rounded-sm flex items-center justify-center cursor-pointer"
           title="Select Post Image"
+          onClick={() => filePickerRef.current.click()}
         >
-          <img
-            src={assets.upload_img}
-            alt="upload_img"
-            className="w-16 h-16 object-cover object-center"
-          />
+          <div className="w-full h-full flex items-center justify-center">
+            <input
+              type="file"
+              accept="image/*"
+              ref={filePickerRef}
+              onChange={(e) => setImage(e.target.files[0])}
+              hidden
+            />
+            <img
+              src={image ? URL.createObjectURL(image) : assets.upload_img}
+              alt="select_img"
+              className={`object-cover object-center ${
+                image ? "w-full h-full" : "w-16 h-16"
+              }`}
+            />
+          </div>
         </div>
 
         {/* 
@@ -59,11 +122,18 @@ const CreatePost = () => {
           Link:- https://www.npmjs.com/package/react-quill-new
 
           npm install react-quill-new --save
+
+          --------------------------------------------------------
+          Link:- https://www.npmjs.com/package/react-quill
+
+          npm i react-quill
         */}
         <ReactQuill
           theme="snow"
           placeholder="Write something..."
           className="w-full h-80"
+          onChange={setContent}
+          value={content}
           required
         />
 
@@ -74,10 +144,11 @@ const CreatePost = () => {
             className={
               "text-textLight bg-bgDark border-none outline-none hover:bg-opacity-[93%]"
             }
-            handleClick={() => alert("Processing...")}
           />
         </div>
       </form>
+
+      {publishError && <p className="text-red-600 mt-6">{publishError}</p>}
     </div>
   );
 };
