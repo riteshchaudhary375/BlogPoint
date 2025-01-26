16d1871de0b514d2eff3328ddeea9d8ca659b83d
ritesh <riteshchaudhary375@gmail.com>
2025-01-26 16:06:27 +0545

    complete message functionality sending from client to mongodb

--- api/index.js
+++ api/index.js
@@ -9,6 +9,7 @@ import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.route.js";
+import messageRoutes from "./routes/message.route.js";
 
 dotenv.config();
 
@@ -39,6 +40,7 @@ app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/comment", commentRoutes);
+app.use("/api/message", messageRoutes);
 
 // Middleware for error handle
 app.use((err, req, res, next) => {
